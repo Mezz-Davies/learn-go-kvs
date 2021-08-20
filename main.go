fdfb908437b621933075bdf0ce45d11b7acf9803
MezzOMG <mezz.davies@sony.com>
2021-08-20 12:35:25 +0100

    Logging methods added

--- main.go
+++ main.go
@@ -21,40 +21,48 @@ type ParsedBody struct {
 func idResponseHandler(w http.ResponseWriter, req *http.Request) {
 	id := strings.TrimPrefix(req.URL.Path, "/kvs/")
 	if len(id) == 0 {
+		log.Println("No id provided.")
 		http.Error(w, fmt.Sprintf("Please provide an ID"), http.StatusBadRequest)
 		return
 	}
 	if isValid, validationError := kvs.IdIsValid(id); !isValid {
+		log.Println("Invalid id provided.")
 		http.Error(w, fmt.Sprintf("ID format error: %s", validationError.Error()), http.StatusBadRequest)
 		return
 	}
-	var v ParsedBody
+	log.Printf("%v Request for id %v\n", req.Method, id)
 	switch req.Method {
 	case "GET":
 		val, err := kvs.Get(id)
 		if err != nil {
+			log.Printf("GET Error %v\n", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
-		fmt.Println(val, "returned from kvs")
 		if val == nil {
-			http.Error(w, "Requested resource does not exist.", http.StatusBadRequest)
+			log.Printf("GET 404: Resource not found.\n")
+			http.Error(w, "Requested resource does not exist.", http.StatusNotFound)
 			return
 		}
 		jsonResult, err := json.Marshal(val)
 		if err != nil {
+			log.Printf("GET JSON formatting Error %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonResult)
 	case "PUT":
+		var v ParsedBody
 		err := json.NewDecoder(req.Body).Decode(&v)
 		if err != nil {
+			log.Printf("PUT JSON decoding Error %v\n", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		err = kvs.Update(id, v)
 		if err != nil {
+			log.Printf("PUT Error %v\n", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -62,6 +70,7 @@ func idResponseHandler(w http.ResponseWriter, req *http.Request) {
 	case "DELETE":
 		err := kvs.Delete(id)
 		if err != nil {
+			log.Printf("DELETE Error %v\n", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -75,21 +84,25 @@ func idResponseHandler(w http.ResponseWriter, req *http.Request) {
 func responseHandler(w http.ResponseWriter, req *http.Request) {
 	var v ParsedBody
 	rMap := make(map[string]interface{})
+	log.Printf("%v Request\n", req.Method)
 	switch req.Method {
 	case "POST":
 		err := json.NewDecoder(req.Body).Decode(&v)
 		if err != nil {
+			log.Printf("POST JSON decoding error %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		id, err := kvs.Set(v)
 		if err != nil {
+			log.Printf("POST Error %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		rMap["id"] = id
 		jsonResult, err := json.Marshal(rMap)
 		if err != nil {
+			log.Printf("POST JSON encoding error %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
